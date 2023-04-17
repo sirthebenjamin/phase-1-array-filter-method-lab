d5d3056ed5ce71abef4d31212846127370d4a006
sirthebenjamin <126521851+sirthebenjamin@users.noreply.github.com>
2023-04-17 13:12:18 -0400

    Adss myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+const drivers = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' },
+    { name: 'Annette', hometown: 'Los Angeles' },
+    { name: 'Bobby', hometown: 'Tampa Bay' }
+  ];
+  
+  function findMatching(drivers, name) {
+    if (!name) {
+      return [];
+    } else {
+      return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+    }
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    if (!letters) {
+      return [];
+    } else {
+      return drivers.filter(driver => driver.startsWith(letters));
+    }
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
